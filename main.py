0d00b9bd739a5c448e81f1269d04d3c043c94420
Olivier Bachard <olivier.bachard@gmail.com>
2026-04-07 12:20:27 +0200

    convert documents to vectors using chroma db

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 
-from src.vector_store import fetch_documents
+from src.vector_store import create_vector_store
+
+from dotenv import load_dotenv
 
 
 def main():
     print("Hello from insur-rag!")
     knowledge_base_path = str(Path(__file__).parent / "knowledge_base")
+    db_path = str(Path(__file__).parent / "chroma_db")
     print(f"Fetching documents from: {knowledge_base_path}")
-    all_text = fetch_documents(knowledge_base_path)
-    print(f"Fetched {len(all_text)} characters from the knowledge base.")
+    vectors_tore = create_vector_store(knowledge_base_path=knowledge_base_path, db_path=db_path)
+    collection = vectors_tore._collection
+    count = collection.count()
+    sample_embedding = collection.get(limit=1, include=["embeddings"])["embeddings"][0]
+    dimension = len(sample_embedding)
+    print(f"Vector store created with {count} documents and embedding dimension of {dimension}.")
 
 
 if __name__ == "__main__":
+    load_dotenv(override=True)
     main()
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,22 +1,49 @@
 import glob
+import os
 from pathlib import Path
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def fetch_documents(knowledge_base_path: Path):
     folders = glob.glob(f"{knowledge_base_path}/*")
+    all_documents = []
     for folder in folders:
         doc_type = Path(folder).name
         print(f"Document type: {doc_type}")
+        # loader = DirectoryLoader(folder, glob="**/*.md")
+        loader = DirectoryLoader(
+            folder,
+            glob="**/*.md",
+            loader_cls=TextLoader,
+            loader_kwargs={"encoding": "utf-8"},
+        )
+        documents = loader.load()
+        for doc in documents:
+            doc.metadata["doc_type"] = doc_type
+            all_documents.append(doc)
 
-    # print(f"Found {len(files)} markdown files in the knowledge base.")
+    return all_documents
 
-    # all_documents = ""
 
-    # for file in files:
-    #     with open(file, "r", encoding="utf-8") as f:
-    #         content = f.read()
-    #         all_documents += content + "\n\n"
+def create_chunks(documents: list) -> list[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(documents)
+    return chunks
 
-    # return all_documents
 
-    return "test"
+def create_vector_store(knowledge_base_path: Path, db_path: Path):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    if os.path.exists(db_path):
+        print("Deleting existing vector store.")
+        Chroma(persist_directory=db_path, embedding_function=embeddings).delete_collection()
+
+    documents = fetch_documents(knowledge_base_path)
+    vector_store = Chroma.from_documents(
+        documents=documents, embedding=embeddings, persist_directory=db_path
+    )
+    return vector_store
